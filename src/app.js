e78d81b2e513dc2a2fb8b907b9d5152c970ad3c3
NelsonJDAS <141588178+NelsonJDAS@users.noreply.github.com>
2024-08-27 20:28:41 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,41 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function num(n) {
+  return Math.floor(Math.random() * n);
+}
+
+function oracion() {
+  let persona = quien[num(quien.length)],
+    acc = accion[num(accion.length)],
+    cos = cosa[num(cosa.length)],
+    cuand = cuando[num(cuando.length)],
+    frase = persona + acc + cos + cuand;
+
+  return frase;
+}
+
+let quien = [
+    "mi abuela",
+    "mi Hámster",
+    "mi Perro",
+    "mi Gato",
+    "mi Pájaro",
+    "mi vecino",
+    "mi amigo",
+    "mi tio",
+    "mi abuelo",
+    "mi novia"
+  ],
+  accion = [" empapo", " perdio", " daño", " tiro", " escondio", " rompio"],
+  cosa = [" mi llave ", " mi telefono ", " mis lentes ", " mi bicicleta "],
+  cuando = ["hoy", "ayer", "hace un segundo", "en la noche", "justo ahora"];
+
+var excusa = document.getElementById("excusa"),
+  boton = document.getElementById("boton");
+
+console.log(excusa);
+boton.addEventListener("click", function() {
+  excusa.innerHTML = "";
+  excusa.innerHTML += `<h1>Nueva Excusa Generada:</h1> <h1 id = "excusa">${oracion()}</h1>`;
+});
